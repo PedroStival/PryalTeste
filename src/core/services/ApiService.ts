efb9f691de4197eb9513785e0bd87409c86c1eef
PedroStival <pedroh.stival@gmail.com>
2021-07-15 12:24:04 -0300

    endpoint

--- src/core/services/ApiService.ts
+++ src/core/services/ApiService.ts
@@ -20,7 +20,7 @@ class ApiService {
     ApiService.vueInstance = app;
     ApiService.vueInstance.use(VueAxios, axios);
     ApiService.vueInstance.axios.defaults.baseURL =
-      "https://localhost:5001/api/";
+      "https://pryalhomol.azurewebsites.net/api/";
     ApiService.vueInstance.axios.defaults.headers.common["Accept"] =
       "application/json";
     ApiService.vueInstance.axios.defaults.headers.common["Content-Type"] =
